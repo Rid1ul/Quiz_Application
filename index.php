c6734dbf1739c54ed25eafc2608d5bbdf4822d82
RIDWAN <islamridwanul42@gmail.com>
2021-03-18 13:39:52 -0800

    First UI added

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Online Quiz</title>
+</head>
+<body>
+<h2>Test your knowledge<h2>
+<p>This is a multiple choice quiz</p>
+
+<ul>
+<li><b>Number of questions:</b> 5</li>
+<li><b>Type:</b> Multiple choice</li>
+<li><b>Estimated time:</b> 4 minues</li>
+</ul>
+
+<a href = "question.php">Start Quiz</a>
+</body>
+
+</html>
+
+
+
 <?php 
 echo "Quiz Application";
 ?>
---
+++ question.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Online Quiz</title>
+</head>
+<body>
+Question 1 of 5
+<p>What does PHP stand for?</p>
+
+<form method="post" action="process.php">
+<ul>
+<li> <input type="radio" id="1" name="1" value ="1" />PHP: Hypertext Preprocessor </li>
+<li> <input type="radio" id="1" name="1" value ="2" />Personal Home Page </li>
+<li> <input type="radio" id="1" name="1" value ="3" />Private Home Page </li>
+<li> <input type="radio" id="1" name="1" value ="4" />Personal Hypertext Preprocessor </li>
+
+</ul>
+
+<input type="submit" value="submit">
+
+
+
+
+</body>
+
+</html>
